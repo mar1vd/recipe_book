ceb10b9963ece807af141bdccdf3a18e8f29c065
SoldierrBoy <ivan.tumchuk@icloud.com>
2025-12-11 21:35:19 +0200

    feat: Redesigned Home page with featured recipe section and stats

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Recipe } from '../../models/recipe.model';
+import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule], 
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  featuredRecipe: Recipe | undefined;
+  totalRecipes = 0;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    const recipes = this.recipeService.getAll();
+    this.totalRecipes = recipes.length;
+
+    if (recipes.length > 0) {
+      const randomIndex = Math.floor(Math.random() * recipes.length);
+      this.featuredRecipe = recipes[randomIndex];
+    }
+  }
+}
